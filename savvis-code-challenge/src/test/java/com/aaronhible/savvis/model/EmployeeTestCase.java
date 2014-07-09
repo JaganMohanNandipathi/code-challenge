36fbd7f31c2a45db97737fc2c5e968825f2ac2f3
R. Aaron Hible <rahible@gmail.com>
2014-07-09 08:09:35 -0500

    DeveloperType allocation 1000, clearer unit tests and test names, remove junit 3 extended class previously added from wizard

--- savvis-code-challenge/src/test/java/com/aaronhible/savvis/model/EmployeeTestCase.java
+++ savvis-code-challenge/src/test/java/com/aaronhible/savvis/model/EmployeeTestCase.java
@@ -1,27 +1,52 @@
 package com.aaronhible.savvis.model;
 
-import java.math.BigDecimal;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import junit.framework.TestCase;
+import java.math.BigDecimal;
 
 import org.junit.Test;
 
-public class EmployeeTestCase extends TestCase {
+public class EmployeeTestCase {
 
 	/**
 	 * UseCase: QA Testers warrant an allocation of $500 each.
 	 */
 	@Test
-	public void qaTesterEmployeeTypeShouldHaveExpenseAllocation() {
+	public void qaTesterTestersWarrantAllocation() {
 
+		// expected
+		BigDecimal expectedAllocation = new BigDecimal(500);
+
+		// setup / action
 		EmployeeId employeeId = new EmployeeId();
 		Employee employee = new Employee(employeeId, new QATesterType());
 
+		// assert
 		assertEquals(employee.getEmployeeId(), employeeId);
 		assertTrue(employee.getEmployeeType() instanceof QATesterType);
-		assertEquals(new BigDecimal(500), employee.getEmployeeType().getExpenseAllocation());
+		assertEquals(expectedAllocation, employee.getEmployeeType()
+				.getExpenseAllocation());
 
 	}
-	
-	
+
+	/**
+	 * UseCase: Developers warrant an allocation of $1000 each.
+	 */
+	@Test
+	public void developersWarrantAllocation() {
+		// expected
+		BigDecimal expectedAllocation = new BigDecimal(1000);
+
+		// setup / action
+		EmployeeId employeeId = new EmployeeId();
+		Employee employee = new Employee(employeeId, new DeveloperType());
+
+		// assert
+		assertEquals(employee.getEmployeeId(), employeeId);
+		assertTrue(employee.getEmployeeType() instanceof DeveloperType);
+		assertEquals(expectedAllocation, employee.getEmployeeType()
+				.getExpenseAllocation());
+	}
+
 }
